4f1f12e810e65b8360067ec55e752dd6d21c0a60
Waheed-Rao <waheednov92@gmail.com>
2024-03-24 02:09:18 +0500

    created no gussing game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env mode
+
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()*6+1);
+const answers = await inquirer.prompt([
+{
+ name: "userguessnumber",
+ type: "number",
+ message: "Please guess a number between 1-6",
+},
+]);
+if(answers.userguessnumber === randomNumber) {
+    console.log( "congratulations! you have guessed right number.");
+} else {
+    console.log( "you have guessed wrong number.");
+}
+console.log(answers);
